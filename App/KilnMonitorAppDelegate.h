da480640c68b6b571d3bac9dcac91d0f10f640ab
Timothy J. Wood <tjw@omnigroup.com>
2010-01-08 22:07:07 -0800

    Some progress on hooking up to a QTCaptureView and trying to process video. Video comes in, but CG emits errors when sending the output to the results view.

--- App/KilnMonitorAppDelegate.h
+++ App/KilnMonitorAppDelegate.h
@@ -13,14 +13,27 @@
     
     NSImage *_sourceImage;
     NSImageView *_sourceImageView;
+    
     NSImageView *_resultImageView;
     
     CIKernel *_detectByGreenKernel;
+    
+    NSArray *_availableCaptureDevices;
+    QTCaptureDevice *_selectedCaptureDevice;
+    NSArrayController *_captureDeviceArrayController;
+    
+    QTCaptureView *_captureView;
+    CIImage *_capturedImage;
+    NSImage *_resultImage;
 }
 
 @property (assign) IBOutlet NSWindow *window;
 @property (retain) IBOutlet NSImageView *sourceImageView;
 @property (retain) IBOutlet NSImageView *resultImageView;
 @property (copy) NSImage *sourceImage;
+
+@property (retain) IBOutlet NSArrayController *captureDeviceArrayController;
+@property (retain) IBOutlet QTCaptureView *captureView;
+@property (readonly) NSImage *resultImage;
 
 @end
--- App/KilnMonitorAppDelegate.m
+++ App/KilnMonitorAppDelegate.m
@@ -8,6 +8,18 @@
 
 #import "KilnMonitorAppDelegate.h"
 
+static NSString * const AvailableCaptureDevicesBinding = @"availableCaptureDevices";
+static NSString * const SelectedCaptureDeviceBinding = @"selectedCaptureDevice";
+static NSString * const CapturedImageBinding = @"capturedImage";
+static NSString * const ResultImageBinding = @"resultImage";
+
+@interface KilnMonitorAppDelegate (/*Private*/)
+@property(copy) NSArray *availableCaptureDevices;
+@property(retain,nonatomic) QTCaptureDevice *selectedCaptureDevice;
+@property(retain) CIImage *capturedImage;
+- (NSImage *)_newResultImage;
+@end
+
 @implementation KilnMonitorAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification;
@@ -25,28 +37,112 @@
         
         _detectByGreenKernel = [[kernels lastObject] retain];
     }
+    
+    self.availableCaptureDevices = [[QTCaptureDevice inputDevicesWithMediaType:QTMediaTypeVideo] retain];
+    NSLog(@"_availableCaptureDevices = %@", _availableCaptureDevices);
 }
 
 @synthesize window = _window;
 @synthesize sourceImage = _sourceImage;
 @synthesize sourceImageView = _sourceImageView;
 @synthesize resultImageView = _resultImageView;
+@synthesize capturedImage = _capturedImage;
+@synthesize resultImage = _resultImage;
 
-+ (NSSet *)keyPathsForValuesAffectingResultImage;
+// Making -resultImage be derived property crashes in NSImageView with a zombie; maybe it calls the accessor w/in an NSAutoreleasePool, not expecting it to go away.
+- (void)setCapturedImage:(CIImage *)capturedImage;
 {
-    return [NSSet setWithObject:@"sourceImage"];
+    if (_capturedImage == capturedImage)
+        return;
+    
+    [self willChangeValueForKey:ResultImageBinding];
+    [_capturedImage release];
+    _capturedImage = [capturedImage retain];
+    [_resultImage release];
+    _resultImage = [self _newResultImage];
+    [self didChangeValueForKey:ResultImageBinding];
 }
-- (NSImage *)resultImage;
+
+@synthesize availableCaptureDevices = _availableCaptureDevices;
+@synthesize captureDeviceArrayController = _captureDeviceArrayController;
+
+@synthesize selectedCaptureDevice = _selectedCaptureDevice;
+- (void)setSelectedCaptureDevice:(QTCaptureDevice *)device;
 {
-    if (!_sourceImage)
+    if (_selectedCaptureDevice == device)
+        return;
+
+    // close any open session and stop using the device.
+    [[_captureView captureSession] stopRunning];
+    [_selectedCaptureDevice close];
+    
+    [self willChangeValueForKey:SelectedCaptureDeviceBinding];
+    [_selectedCaptureDevice release];
+    _selectedCaptureDevice = [device retain];
+    [self didChangeValueForKey:SelectedCaptureDeviceBinding];
+    
+    NSLog(@"_selectedCaptureDevice = %p %@", _selectedCaptureDevice);
+    
+    NSLog(@"captureView.session = %@", [_captureView captureSession]);
+    NSLog(@"captureView.availableVideoPreviewConnections = %@", [_captureView availableVideoPreviewConnections]);
+    
+    QTCaptureSession *captureSession = nil;
+    
+    if (_selectedCaptureDevice) {
+        NSError *error = nil;
+
+        if (![_selectedCaptureDevice open:&error]) {
+            NSLog(@"Unable to open device %@", error);
+            return;
+        }
+        NSLog(@"opened");
+        
+        captureSession = [[[QTCaptureSession alloc] init] autorelease];
+        QTCaptureDeviceInput *captureVideoDeviceInput = [[[QTCaptureDeviceInput alloc] initWithDevice:_selectedCaptureDevice] autorelease];
+        
+        if (![captureSession addInput:captureVideoDeviceInput error:&error]) {
+            NSLog(@"error adding input %@", error);
+            return;
+        }
+        NSLog(@"input added");
+    }
+    
+    [_captureView setCaptureSession:captureSession];
+    
+    if (captureSession) {
+        [captureSession startRunning];
+        NSLog(@"started");
+    }
+}
+
+@synthesize captureView = _captureView;
+
+#pragma mark -
+#pragma mark QTCaptureView delegate
+
+- (CIImage *)view:(QTCaptureView *)view willDisplayImage:(CIImage *)image;
+{
+    self.capturedImage = image;
+    return nil; // use the passed in image
+}
+
+#pragma mark -
+#pragma mark Private
+
+- (NSImage *)_newResultImage;
+{
+    if (!_capturedImage)
         return nil;
     
+#if 0
     CIImage *sourceCIImage;
     {
-        CGImageRef sourceImageRef = [_sourceImage CGImageForProposedRect:NULL context:nil hints:nil];
-        sourceCIImage = [[CIImage alloc] initWithCGImage:sourceImageRef];
+        CGImageRef sourceImageRef = [_sourceImage CGImageForProposedRect:NULL context:[window graphicsContext] hints:nil];
+        sourceCIImage = [[[CIImage alloc] initWithCGImage:sourceImageRef] autorelease];
         CGImageRelease(sourceImageRef);
     }
+#endif
+    //    CIImage *sourceCIImage = _capturedImage;
     
     CIImage *resultCIImage;
     {
@@ -54,15 +150,15 @@
         [blurFilter setDefaults];
         
         [blurFilter setValue:[NSNumber numberWithDouble:8.0f] forKey:@"inputRadius"];
-        [blurFilter setValue:sourceCIImage forKey:@"inputImage"];
+        [blurFilter setValue:_capturedImage forKey:@"inputImage"];
         
         CIImage *blurImage = [blurFilter valueForKey:@"outputImage"];
         
         CIFilter *detectGreenFilter = [[CIFilter alloc] init];
         CISampler *blurSampler = [CISampler samplerWithImage:blurImage];
         CIImage *greenDetectImage = [detectGreenFilter apply:_detectByGreenKernel, blurSampler, kCIApplyOptionDefinition, [blurImage definition], nil];
-
-        NSLog(@"greenDetectImage = %@", greenDetectImage);
+        
+        //NSLog(@"greenDetectImage = %@", greenDetectImage);
         [detectGreenFilter release];
         
         resultCIImage = greenDetectImage;
@@ -71,9 +167,8 @@
     NSImage *resultImage;
     {
         NSCIImageRep *ciImageRep = [[NSCIImageRep alloc] initWithCIImage:resultCIImage];
-        [resultCIImage release];
-    
-        resultImage = [[NSImage alloc] initWithSize:[_sourceImage size]];
+        
+        resultImage = [[NSImage alloc] initWithSize:[ciImageRep size]]; // LEAK
         [resultImage addRepresentation:ciImageRep];
         [ciImageRep release];
     }
